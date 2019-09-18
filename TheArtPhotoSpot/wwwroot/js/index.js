e4105c21108916c5f45fa0066eee27231a47d17f
nenadnesta <nenadnesta@gmail.com>
2019-09-18 23:12:10 +0200

    wwwroot - css, img, js.

---
+++ TheArtPhotoSpot/wwwroot/js/index.js
@@ -0,0 +1,12 @@
+﻿var x = 0;
+var s = "";
+
+console.log("Hello World!");
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button, addEventListener("click", function () {
+    console.log("Buying Item");
+});
