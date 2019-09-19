61fc2c741678bc8dfcfba6cd23941d669a31e51b
nenadnesta <nenadnesta@gmail.com>
2019-09-19 18:07:57 +0200

    Enabling MVC.

--- TheArtPhotoSpot/Startup.cs
+++ TheArtPhotoSpot/Startup.cs
@@ -13,12 +13,16 @@ namespace TheArtPhotoSpot
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
             // For wwwroot directory
             app.UseStaticFiles();
@@ -33,6 +37,13 @@ namespace TheArtPhotoSpot
                     RequestPath = new PathString("/vendor")
                 });
             }
+
+            app.UseMvc(cfg =>
+            {
+                cfg.MapRoute("Default",
+                  "{controller}/{action}/{id?}",
+                  new { controller = "App", Action = "Index" });
+            });
         }
     }
 }
