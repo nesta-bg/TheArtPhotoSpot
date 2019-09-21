75b87f60ff405618c59e046b400d2a53cce64021
nenadnesta <nenadnesta@gmail.com>
2019-09-21 14:31:15 +0200

    Creating Entities.

---
+++ TheArtPhotoSpot/Data/ArtContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheArtPhotoSpot.Data.Entites;
+
+namespace TheArtPhotoSpot.Data
+{
+    public class ArtContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
