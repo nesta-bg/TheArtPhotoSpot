46867e449af88e7ae5a50d098b6055a516c33f97
nenadnesta <nenadnesta@gmail.com>
2019-09-21 15:43:41 +0200

    Using DbContext - AppController Shop.

--- TheArtPhotoSpot/Controllers/AppController.cs
+++ TheArtPhotoSpot/Controllers/AppController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using TheArtPhotoSpot.Data;
 using TheArtPhotoSpot.ViewModels;
 
 namespace TheArtPhotoSpot.Controllers
 {
     public class AppController : Controller
     {
+        private readonly ArtContext _context;
+
+        public AppController(ArtContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -36,6 +45,23 @@ namespace TheArtPhotoSpot.Controllers
             ViewBag.Title = "About Us";
 
             return View();
+        }
+
+        public IActionResult Shop()
+        {
+            //LINQ Query
+            //var results = from p in _context.Products
+            //              orderby p.Category
+            //              select p;
+
+            //return View(results.ToList());
+
+
+            var results = _context.Products
+                .OrderBy(p => p.Category)
+                .ToList();
+
+            return View(results);
         }
     }
 }
