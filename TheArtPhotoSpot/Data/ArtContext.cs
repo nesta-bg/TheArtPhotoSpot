7aafe70d517eb38fb81e387dd27523d95325e32a
nenadnesta <nenadnesta@gmail.com>
2019-09-21 15:10:54 +0200

    Entity Framework Core, Configuration.

--- TheArtPhotoSpot/Data/ArtContext.cs
+++ TheArtPhotoSpot/Data/ArtContext.cs
@@ -3,8 +3,13 @@ using TheArtPhotoSpot.Data.Entites;
 
 namespace TheArtPhotoSpot.Data
 {
-    public class ArtContext
+    public class ArtContext : DbContext
     {
+        public ArtContext(DbContextOptions<ArtContext> options) : base(options)
+        {
+
+        }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
     }
--- TheArtPhotoSpot/Startup.cs
+++ TheArtPhotoSpot/Startup.cs
@@ -1,18 +1,33 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using System.IO;
+using TheArtPhotoSpot.Data;
 
 namespace TheArtPhotoSpot
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ArtContext>(cfg =>
+            {
+                cfg.UseSqlServer(_config.GetConnectionString("ArtConnectionString"));
+            });
+
             services.AddMvc();
         }
 
