307efdcc5b82db02d5aaa4d98d7da8dc45aa0678
nenadnesta <nenadnesta@gmail.com>
2019-09-26 09:23:04 +0200

    ProductsController - API.

---
+++ TheArtPhotoSpot/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using System;
+using TheArtPhotoSpot.Data;
+
+namespace TheArtPhotoSpot.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IArtRepository _repository;
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(IArtRepository repository, ILogger<ProductsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_repository.GetAllProducts());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get products: {ex}");
+                return BadRequest("Failed to get products");
+            }
+        }
+    }
+}
