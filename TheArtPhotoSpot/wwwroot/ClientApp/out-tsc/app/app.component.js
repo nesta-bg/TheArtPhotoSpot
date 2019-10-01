999fe3ce20b6ea2ef1444d996222f564a95ed216
nenadnesta <nenadnesta@gmail.com>
2019-10-01 13:17:33 +0200

    Angular Application Adjustment.

---
+++ TheArtPhotoSpot/wwwroot/ClientApp/out-tsc/app/app.component.js
@@ -0,0 +1,20 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+let AppComponent = class AppComponent {
+    constructor() {
+        this.title = 'The Art Photo Spot';
+    }
+};
+AppComponent = tslib_1.__decorate([
+    Component({
+        selector: 'app-root',
+        template: `
+      <h1>
+        Welcome to {{title}}!
+      </h1>
+  `,
+        styles: []
+    })
+], AppComponent);
+export { AppComponent };
+//# sourceMappingURL=app.component.js.map
